203bd2f5d04c4da0285601acb3a50ebb76a1f68e
Jordi Prats <jordi.prats@gmail.com>
2020-07-25 19:55:05 +0200

    how to upload

--- upload.sh
+++ upload.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$1" ] || [ -z "$2" ];
+then
+    echo $0 <SSID> <PASSW0RD>
+fi
+
 cat main_template.py | sed "s/_SSID_/$1/" | sed "s/_PASSWORD_/$2/" > main.py
 pkill screen
 ampy --port /dev/ttyUSB0 rm main.py
